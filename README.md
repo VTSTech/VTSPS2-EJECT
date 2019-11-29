67e607272a7e40f4f0997a628aee283ab89a62d2
Nigel Todman <veritas@vts-tech.org>
2019-11-29 18:11:19 -0500

    v0.2
    Code optimizations

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<img src="https://i.gyazo.com/aa1cadd9c243d3b16d6cbd57d5211f7c.png">
+
 # PS2Eject
  A simple homebrew to eject the disc tray of a PS2
+ 
+ v0.2 11/29/2019 3:09:20 PM
+ Code optimizations
+
+ v0.1 11/29/2019 11:44:31 AM
+ First release
--- eject.c
+++ eject.c
@@ -1,6 +1,9 @@
 // A simple homebrew to eject the disc tray of a PS2
 // PS2Eject v0.1 Written by VTSTech (veritas@vts-tech.org)
 
+// v0.2 11/29/2019 3:09:20 PM
+// Code optimizations
+
 // v0.1 11/29/2019 11:44:31 AM
 // First release
 
@@ -8,40 +11,37 @@
 #include <unistd.h>
 #include <libcdvd.h>
 
+int DriveState()
+{
+  int CdStatus;
+  sleep(1);
+  CdStatus = sceCdStatus();
+  if (CdStatus == 0) {
+  	scr_printf("* sceCdvdDriveState: (00) SCECdStatStop\n");
+  } else if (CdStatus == 1) {
+  	scr_printf("* sceCdvdDriveState: (01) SCECdStatShellOpen\n");
+  } else if (CdStatus == 10) {
+  	scr_printf("* sceCdvdDriveState: (10) SCECdStatSpin\n");
+  } else {
+  	scr_printf("* sceCdvdDriveState: (%d) Unknown\n",CdStatus);
+  }
+  return 0;
+}
+
 int main()
 {
-  int num;
-  char str;
+  int TrayCheck;
   init_scr();
   scr_clear();
   sleep(1);
-  scr_printf("PS2Eject v0.1 by VTSTech\n");
-  scr_printf("========================\n\n");
-  if (sceCdInit(SCECdINoD) == 1) { scr_printf("* libcdvd initalized...\n"); }
-  sleep(1);
-  num = sceCdStatus();
-  if (num == 0) {
-  	scr_printf("* SCECdvdDriveState: (0) SCECdStatStop\n");
-  } else if (num >=1) {
-  	scr_printf("* SCECdvdDriveState: (%d) Unknown\n",num);
-  }
-  scr_printf("* sceCdTrayReq(3,SCECdTrayCheck): %d\n",sceCdTrayReq(3,num));
-  scr_printf("* SCECdTrayCheck: (%d) Unknown\n",num);
-  sleep(1);
-  num = sceCdStatus();
-  if (num == 0) {
-  	scr_printf("* SCECdvdDriveState: (0) SCECdStatStop\n");
-  } else if (num >=1) {
-  	scr_printf("* SCECdvdDriveState: (%d) Unknown\n",num);
-  }
-  scr_printf("* sceCdTrayReq(0,SCECdTrayCheck): %d\n",sceCdTrayReq(0,num));
-  scr_printf("* SCECdTrayCheck: (%d) Unknown\n",num);
-  num = sceCdStatus();
-  if (num == 0) {
-  	scr_printf("* SCECdvdDriveState: (0) SCECdStatStop\n");
-  } else if (num >=1) {
-  	scr_printf("* SCECdvdDriveState: (%d) Unknown\n",num);
-  }
+  scr_printf("PS2Eject v0.2 by VTSTech (11-29-2019)\n=====================================\n\n");
+  if (sceCdInit(SCECdINoD) == 1) { scr_printf("* libcdvd initalized...\n\n"); }
+	DriveState();
+  if (sceCdTrayReq(3,&TrayCheck) == 1) { scr_printf("* sceCdTrayReq(3,SCECdTrayCheck): %d\n",TrayCheck); } else { scr_printf("! sceCdTrayReq() failed"); }
+	DriveState();
+	if (sceCdTrayReq(0,&TrayCheck) == 1) { scr_printf("* sceCdTrayReq(0,SCECdTrayCheck): %d\n",TrayCheck); } else { scr_printf("! sceCdTrayReq() failed"); }
+	DriveState();
+  scr_printf("\n* All operations complete. Exit in 30s...");
   sleep(30);
   return 0;
 }
