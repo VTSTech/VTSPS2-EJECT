78cc5fcaf10493e451449ee02c235069c0f84074
Nigel Todman <veritas@vts-tech.org>
2019-11-29 18:29:29 -0500

    v0.2
    spacing/identation

--- eject.c
+++ eject.c
@@ -19,29 +19,39 @@ int DriveState()
   if (CdStatus == 0) {
   	scr_printf("* sceCdvdDriveState: (00) SCECdStatStop\n");
   } else if (CdStatus == 1) {
-  	scr_printf("* sceCdvdDriveState: (01) SCECdStatShellOpen\n");
+  		scr_printf("* sceCdvdDriveState: (01) SCECdStatShellOpen\n");
   } else if (CdStatus == 10) {
-  	scr_printf("* sceCdvdDriveState: (10) SCECdStatSpin\n");
+  		scr_printf("* sceCdvdDriveState: (10) SCECdStatSpin\n");
   } else {
-  	scr_printf("* sceCdvdDriveState: (%d) Unknown\n",CdStatus);
+  		scr_printf("* sceCdvdDriveState: (%d) Unknown\n",CdStatus);
   }
   return 0;
 }
 
 int main()
 {
-  int TrayCheck;
-  init_scr();
-  scr_clear();
-  sleep(1);
-  scr_printf("PS2Eject v0.2 by VTSTech (11-29-2019)\n=====================================\n\n");
-  if (sceCdInit(SCECdINoD) == 1) { scr_printf("* libcdvd initalized...\n\n"); }
+	int TrayCheck;
+	init_scr();
+	scr_clear();
+	sleep(1);
+	scr_printf("PS2Eject v0.2 by VTSTech (11-29-2019)\n=====================================\n\n");
+	if (sceCdInit(SCECdINoD) == 1) {
+		scr_printf("* libcdvd initalized...\n\n");
+		}
 	DriveState();
-  if (sceCdTrayReq(3,&TrayCheck) == 1) { scr_printf("* sceCdTrayReq(3,SCECdTrayCheck): %d\n",TrayCheck); } else { scr_printf("! sceCdTrayReq() failed"); }
+	if (sceCdTrayReq(3,&TrayCheck) == 1) {
+		scr_printf("* sceCdTrayReq(3,SCECdTrayCheck): %d\n",TrayCheck);
+		} else {
+			scr_printf("! sceCdTrayReq() failed");
+		}
 	DriveState();
-	if (sceCdTrayReq(0,&TrayCheck) == 1) { scr_printf("* sceCdTrayReq(0,SCECdTrayCheck): %d\n",TrayCheck); } else { scr_printf("! sceCdTrayReq() failed"); }
+	if (sceCdTrayReq(0,&TrayCheck) == 1) {
+		scr_printf("* sceCdTrayReq(0,SCECdTrayCheck): %d\n",TrayCheck);
+		} else {
+			scr_printf("! sceCdTrayReq() failed");
+		}
 	DriveState();
-  scr_printf("\n* All operations complete. Exit in 30s...");
-  sleep(30);
-  return 0;
+	scr_printf("\n* All operations complete. Exit in 30s...");
+	sleep(30);
+	return 0;
 }
